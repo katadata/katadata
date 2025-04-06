64a500e9fadec1ff89e32b2c743bf642a23dac46
katadata <katarzyna.licznerska1@gmail.com>
2025-04-06 15:42:08 +0200

    Add files via upload

---
+++ non-first-orders.sql
@@ -0,0 +1,45 @@
+/* which products generated the most non-first orders */
+with 
+order_ranking as
+	(
+	select
+		order_id
+		,customer_id 
+		,row_number() over (partition by customer_id order by order_id) as ranked_order
+	from orders
+	),
+orders_made as
+	(
+	select
+		o.order_id
+		,o.customer_id
+		,op.product_id
+		,op.order_position_id
+		,ora.ranked_order
+		,max(ora.ranked_order) over (partition by customer_id)-1 as non_first_order_made
+	from orders o
+	join order_positions op on o.order_id = op.order_id
+	join order_ranking 	ora	on o.order_id = ora.order_id
+	order by 2, 1
+	),
+prep_agg as 
+(
+select distinct
+	order_id
+	,product_id
+	,non_first_order_made
+from orders_made
+where ranked_order = 1
+)
+select
+	product_id
+	,sum(non_first_order_made) as all_non_first_orders_made
+from prep_agg
+group by 1
+order by 2 desc
+
+select
+product_name
+from products
+where product_id = 1162
+
---
+++ orders_tasks.sql
@@ -0,0 +1,129 @@
+/* 1. Liczba zamówień i unikalnych klientów/Number of orders and number of unique customers */
+select 
+count(order_id) as nr_of_orders
+,count(distinct customer_id) as nr_of_customers
+from orders
+/* Mamy 5009 zamówień oraz 793 unikalnych klientów/We have 5009 orders and 793 unique customers */
+
+/* 2. Średni czas dostawy/ Average delivery time */
+select 
+round(avg(DATEDIFF(shipping_date, order_date))) as avg_of_time_order
+from orders
+/* Średni czas dostawy wynosi 4 dni/ Average delivery time is 4 days */
+
+/* 3. Liczba zamówień według miasta */
+select
+delivery_city
+,count(order_id) as nr_of_orders
+from orders
+group by 1
+order by 2 desc
+
+/* 3A. Dla wybranych miast, zaczynających się na C */
+select
+delivery_city
+,count(order_id) as nr_of_orders
+from orders
+where delivery_city like "C%"
+group by 1
+order by 2 desc
+
+/* 3B. Dla wybranych miast: "Houston, Philadelphia" */
+select
+delivery_city
+,count(order_id) as nr_of_orders
+from orders
+where delivery_city in ("Houston", "Philadelphia")
+group by 1
+order by 2 desc
+
+ /* 3C. Procentowy udział zamówień z danego kraju/
+percentage of orders by country */
+select
+	delivery_city
+	,count(order_id) as nr_of_orders
+	,round((count(order_id)/(select count(order_id) from orders))*100,2) as percentage_of_orders
+from orders
+group by 1
+order by 2 desc
+
+/*4. Miasta z największą liczbą zamówień/ Cities with the highest number of orders */
+select
+delivery_city
+,count(order_id) as nr_of_orders
+from orders
+group by 1
+order by 2 desc
+limit 5
+
+/* Cities with the highest number of orders are: New York City, Los Angeles, Philadelphia, San Francisco, Seattle
+
+/* 6. Liczba zamówień w każdym miesiącu/ Number of orders per month */
+select
+	year(order_date) 	as order_year
+	,month(order_date) 	as order_month
+	,count(*) 			as nr_of_orders
+from orders
+group by 1,2
+order by 1,2
+
+/* 7. Klienci z największą liczbą zamówień/ Customers with the highest number of orders (top 10) */
+select
+	customer_id
+	,count(order_id) as nr_of_orders
+from orders
+group by 1
+order by 2 desc
+limit 10
+
+/*8.  Liczba zamówień zrealizowanych tego samego dnia w podziale na rok i miesiąc/ Number of orders shipped on the same day */
+
+select
+	year(order_date) as year_of_order
+	,month(order_date) as month_of_order
+	,count(order_id) as nr_of_orders
+from orders
+where shipping_date = order_date
+group by 1,2
+order by 1,2
+
+/*  Procent zamówień wysłanych tego samego dnia względem wszystkich zamówień/ Percentage of orders shipped on the same day relative to all orders */
+with total_orders as
+	(
+	select count(order_id) as total
+	from orders
+	) 
+select 
+	count(o.order_id) as nr_of_orders
+	,tor.total as total
+	,round((count(o.order_id)/tor.total)*100,2) as percentage
+from total_orders tor
+cross join orders o
+where shipping_date=order_date
+group by tor.total
+
+/* 9. Zamówienia per klient - histogram/ Orders per customer – histogram */
+with
+orders_per_customer as
+	(
+	select 
+		customer_id
+		,count(order_id) as orders
+	from orders
+	group by 1
+	)
+select 
+	orders
+	,count(customer_id) as num_of_customers
+	,round(count(customer_id)/(select count(distinct customer_id) from orders)*100,2) as percent_of_customers
+from orders_per_customer
+group by 1
+order by 1
+
+/* 10. Średni czas dostawy dla każdego kraju/ Average delivery time per country */
+select
+delivery_city
+,round(avg(datediff(shipping_date, order_date))) as avg_delivery
+from orders
+group by 1
+order by 2
